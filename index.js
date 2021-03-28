d77affe4b08d8762d7568ccb4979cbb12ed92943
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-03-28 16:31:42 +0100

    added prettier and formatted documents

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const getFibonacciSequence = (nums) => {
-  if(nums < 0 ){ throw(new Error('nums is negative')); }
-  if(nums === 0 ){ return []; }
-  let fibA = 0, fibB = 1;
-  if (nums === 1){ return [fibA]; }
-  const fibNums = [fibA, fibB];
-  for (let i = 2; i < nums; i++) {
-    const tmpFib = fibA + fibB;
-    fibNums.push(tmpFib);
-    fibA = fibB;
-    fibB = tmpFib;
-  }
-  return fibNums;
+    if (nums < 0) {
+        throw new Error('nums is negative');
+    }
+    if (nums === 0) {
+        return [];
+    }
+    let fibA = 0,
+        fibB = 1;
+    if (nums === 1) {
+        return [fibA];
+    }
+    const fibNums = [fibA, fibB];
+    for (let i = 2; i < nums; i++) {
+        const tmpFib = fibA + fibB;
+        fibNums.push(tmpFib);
+        fibA = fibB;
+        fibB = tmpFib;
+    }
+    return fibNums;
 };
 
 const numbersToGet = 2;
 const fibSequence = getFibonacciSequence(numbersToGet);
 let outStr = `The first ${numbersToGet} fibonacci numbers are: `;
 fibSequence.forEach(
-  (fib, idx) => (outStr += `${fib}${idx < fibSequence.length - 1 ? ', ' : ''}`)
+    (fib, idx) =>
+        (outStr += `${fib}${idx < fibSequence.length - 1 ? ', ' : ''}`)
 );
 console.log(outStr);
 
