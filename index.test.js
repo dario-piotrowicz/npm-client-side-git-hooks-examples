dcc35bbaa4c3616070a6cf27724abb0521db2935
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-03-28 16:14:00 +0100

    implemented dummy index.js and jest tests

---
+++ index.test.js
@@ -0,0 +1,25 @@
+const getFibonacciSequence = require('./index');
+
+
+test('getFibonacciSequence(n) should return n numbers', () => {
+  const test = (n) => {
+    let fibNums = getFibonacciSequence(n);
+    expect(fibNums.length).toEqual(n);
+  }
+
+  [0,1,2,3,4,5,90,200].forEach( n => test(n) );
+});
+
+
+test('getFibonacciSequence should throw an error is the argument is negative', () => {
+
+
+    expect(() => getFibonacciSequence(-1)).toThrow();
+
+    // const test = (n) => {
+    //   let fibNums = getFibonacciSequence(n);
+    //   expect(fibNums.length).toEqual(n);
+    // }
+  
+    // [0,1,2,3,4,5,90,200].forEach( n => test(n) );
+});
