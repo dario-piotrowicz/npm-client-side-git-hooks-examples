2debf1f56bd01798a495204db09a35f6c5e5ca85
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-03-28 16:23:05 +0100

    added eslint

--- index.test.js
+++ index.test.js
@@ -12,14 +12,5 @@ test('getFibonacciSequence(n) should return n numbers', () => {
 
 
 test('getFibonacciSequence should throw an error is the argument is negative', () => {
-
-
     expect(() => getFibonacciSequence(-1)).toThrow();
-
-    // const test = (n) => {
-    //   let fibNums = getFibonacciSequence(n);
-    //   expect(fibNums.length).toEqual(n);
-    // }
-  
-    // [0,1,2,3,4,5,90,200].forEach( n => test(n) );
 });
